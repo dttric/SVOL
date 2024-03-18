11f882c429a0ab9d64697ba8d0de7d5e0e074a41
dttric <87319664+dttric@users.noreply.github.com>
2024-03-18 22:45:04 +0700

    я знаю что это никто не читает

--- svol/src/main.rs
+++ svol/src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+/* правила компилятора */
+#![allow(unused_imports)] // РАЗРАБ
+#![allow(unused_variables)] // ДАУН
+
+/* импорты */
+use std::thread;
+use std::time::Duration;
+use console::Term;
+use console::Style;
+use colorized::*;
+
+/* функции */
+fn windows() -> bool
+{
+  if cfg!(target_os = "windows"){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+fn ret_error(text: &str)
+{
+  println!("[ERROR] {}", text.color(Colors::RedBg))
+}
+
+fn ret_success(text: &str)
+{
+  println!("[SUCCESS] {}", text.color(Colors::GreenBg).color(Colors::BlackFg))
+}
+
+fn log(text: &str)
+{
+  println!("[LOG] {}", text.color(Colors::YellowBg).color(Colors::BlackFg))
+}
+
+fn main()
+{
+  /* проверка на винду */
+  if windows() == false {
+    ret_error("Запустите программу на Windows!");
+  } else {
+    ret_success("Ошибок не обнаружено...");
+    log("Запускаюсь...")
+  }
 }
