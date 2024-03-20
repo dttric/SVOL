007fbdee0ad88bf935439a00fd9df6b2b75c7ab7
dttric <87319664+dttric@users.noreply.github.com>
2024-03-20 22:31:38 +0700

    окна

--- src/format.rs
+++ src/format.rs
@@ -2,7 +2,11 @@
 use colorized::*;
 use std::process::exit;
 pub fn error(text: &str) { println!("[ERROR] {}", text.color(Colors::RedBg)); }
-pub fn success(text: &str) { println!("[SUCCESS] {}", text.color(Colors::GreenBg).color(Colors::BlackFg)); }
+pub fn success(logtype: &str, text: &str) { println!("[{}] {}", logtype, text.color(Colors::GreenBg).color(Colors::BlackFg)); }
 pub fn fatal(text: &str) { panic!("[FATAL] {}", text.color(Colors::RedBg)); }
-pub fn log(text: &str) { println!("[LOG] {}", text.color(Colors::YellowBg).color(Colors::BlackFg)); }
+pub fn log(logtype: &str, text: &str) { println!("[{}] {}", logtype, text.color(Colors::YellowBg).color(Colors::BlackFg)); }
 pub fn warn(text: &str) { println!("[WARN] {}", text.color(Colors::YellowBg).color(Colors::BlackFg)); }
+pub fn logo() -> String
+{
+    return "─────────────────────────────────────────────────────────────\n─██████████████─██████──██████─██████████████─██████────────\n─██░░░░░░░░░░██─██░░██──██░░██─██░░░░░░░░░░██─██░░██─────────\n─██░░██████████─██░░██──██░░██─██░░██████░░██─██░░██─────────\n─██░░██─────────██░░██──██░░██─██░░██──██░░██─██░░██─────────\n─██░░██████████─██░░██──██░░██─██░░██──██░░██─██░░██─────────\n─██░░░░░░░░░░██─██░░██──██░░██─██░░██──██░░██─██░░██─────────\n─██████████░░██─██░░██──██░░██─██░░██──██░░██─██░░██─────────\n─────────██░░██─██░░░░██░░░░██─██░░██──██░░██─██░░██─────────\n─██████████░░██─████░░░░░░████─██░░██████░░██─██░░██████████─\n─██░░░░░░░░░░██───████░░████───██░░░░░░░░░░██─██░░░░░░░░░░██─\n─██████████████─────██████─────██████████████─██████████████─\n─────────────────────────────────────────────────────────────".to_string()
+}
--- src/window.rs
+++ src/window.rs
@@ -1,16 +1,20 @@
-#![allow(dead_code)]
-#![allow(unused_must_use)]
-use cli_gui::*;
-use std::process::exit;
-use console::Term;
-pub fn init() {
-  let term = Term::stdout();
-  let mut window = Window::new(Position::new(1, 0), Size::new(100, 50));
-  window.set_to_main();
-  window.write(Position::new(0, 0), "Hello".to_string(), Color::new(100, 255, 50));
-  window.decorate();
-  window.render();
-  window.read_line(Position::new(10, 10), "input: ", Color::new(100, 100, 100), true);
-  term.clear_screen();
-  // window.quit();
+extern crate pancurses;
+use pancurses::{ALL_MOUSE_EVENTS, endwin, getmouse, initscr, mousemask, Input, noecho};
+
+pub fn init()
+{
+  let window = initscr();
+  window.printw("Welcome to SVOL!");
+  window.refresh();
+  window.keypad(true);
+  noecho();
+  loop {
+      match window.getch() {
+          Some(Input::Character(c)) => { window.addch(c); },
+          Some(Input::KeyDC) => break,
+          Some(input) => { window.addstr(&format!("{:?}", input)); },
+          None => ()
+      }
+  }
+  endwin();
 }
